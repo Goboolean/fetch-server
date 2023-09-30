1e03461f98fdc837923b159fd586cf8a3eda0217
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-10-01 03:51:16 +0900

    remove unused code

--- internal/infrastructure/etcd/util/marshal_test.go
+++ internal/infrastructure/etcd/util/marshal_test.go
@@ -26,77 +26,7 @@ func Test_GroupBy(t *testing.T) {
 		list   map[string]string
 		prefix string
 	}
-/*
-	tests := []struct {
-		name    string
-		args    args
-		want    []map[string]string
-		wantLen int
-	} {
-		{
-			name: "Worker",
-			args: args{
-				list: map[string]string{
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
-				},
-				prefix: "worker",
-			},
-			want: []map[string]string{
-				{
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
-				},
-				{
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
-				},
-			},
-			wantLen: 2,
-		},
-		{
-			name: "Product",
-			args: args{
-				list: map[string]string{
-					"product/test.goboolean.kor": "",
-					"product/test.goboolean.kor/platform": "kis",
-					"product/test.goboolean.kor/symbol": "goboolean",
-					"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					"product/test.goboolean.kor/status": "onsubscribe",
-					"product/test.goboolean.eng": "",
-					"product/test.goboolean.eng/platform": "kis",
-					"product/test.goboolean.eng/symbol": "goboolean",
-					"product/test.goboolean.eng/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					"product/test.goboolean.eng/status": "onsubscribe",
-				},
-				prefix: "product",
-			},
-			want: []map[string]string{
-				{
-					"product/test.goboolean.kor": "",
-					"product/test.goboolean.kor/platform": "kis",
-					"product/test.goboolean.kor/symbol": "goboolean",
-					"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					"product/test.goboolean.kor/status": "onsubscribe",
-				},
-				{
-					"product/test.goboolean.eng": "",
-					"product/test.goboolean.eng/platform": "kis",
-					"product/test.goboolean.eng/symbol": "goboolean",
-					"product/test.goboolean.eng/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					"product/test.goboolean.eng/status": "onsubscribe",
-				},
-			},
-			wantLen: 2,
-		},
-	}
-*/
+
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := etcdutil.GroupBy(tt.whole)
