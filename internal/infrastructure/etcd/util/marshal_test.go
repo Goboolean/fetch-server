660a683d6a67597c8a9dddbdb68ee61c4f6d2414
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-10-01 00:06:58 +0900

    refactor test code to sync test cases of marshal and unmarshal function

--- internal/infrastructure/etcd/util/marshal_test.go
+++ internal/infrastructure/etcd/util/marshal_test.go
@@ -1,6 +1,8 @@
 package etcdutil_test
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"reflect"
 	"testing"
@@ -110,115 +112,135 @@ func Test_GroupBy(t *testing.T) {
 }
 
 
+var cases []struct {
+	name string
+	str map[string]string
+	model interface{}
+	data interface{}
+} = []struct{
+	name string
+	str map[string]string
+	model interface{}
+	data interface{}
+}{
+	{
+		name: "Worker",
+		str: map[string]string{
+			"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
+			"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
+			"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "active",
+		},
+		model: struct{
+			ID       string `etcd:"id"`
+			Platform string `etcd:"platform"`
+			Status   string `etcd:"status"`
+		} {},
+		data: struct{
+			ID       string `etcd:"id"`
+			Platform string `etcd:"platform"`
+			Status   string `etcd:"status"`
+		} {
+			ID: "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+			Platform: "kis",
+			Status: "active",
+		},
+	},
+	{
+		name: "Product",
+		str: map[string]string{
+			"product/test.goboolean.kor": "",
+			"product/test.goboolean.kor/platform": "kis",
+			"product/test.goboolean.kor/symbol": "goboolean",
+			"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+			"product/test.goboolean.kor/status": "onsubscribe",
+		},
+		model: struct{
+			ID       string `etcd:"id"`
+			Platform string `etcd:"platform"`
+			Symbol   string `etcd:"symbol"`
+			Worker 	 string `etcd:"worker"`
+			Status   string `etcd:"status"`
+		} {},
+		data: struct{
+			ID       string `etcd:"id"`
+			Platform string `etcd:"platform"`
+			Symbol   string `etcd:"symbol"`
+			Worker 	 string `etcd:"worker"`
+			Status   string `etcd:"status"`
+		} {
+			ID: "test.goboolean.kor",
+			Platform: "kis",
+			Symbol: "goboolean",
+			Worker: "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+			Status: "onsubscribe",
+		},
+	},
+	{
+		name: "Nested Struct",
+		str: map[string]string{
+			"nested/mulmuri.dev": "",
+			"nested/mulmuri.dev/detail": "",
+			"nested/mulmuri.dev/detail/name": "goboolean",
+			"nested/mulmuri.dev/detail/age": "1",
+		},
+		model: struct{
+			ID string `etcd:"id"`
+			Detail struct{
+				Name string `etcd:"name"`
+				Age int `etcd:"age"`
+			} `etcd:"detail"`
+		} {},
+		data: struct{
+			ID string `etcd:"id"`
+			Detail struct{
+				Name string `etcd:"name"`
+				Age int `etcd:"age"`
+			} `etcd:"detail"`
+		} {
+			ID: "mulmuri.dev",
+			Detail: struct{
+				Name string `etcd:"name"`
+				Age int `etcd:"age"`
+			} {
+				Name: "goboolean",
+				Age: 1,
+			},
+		},
+	},
+}
+
+func deepCopy(src, dst interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(&buf).Decode(dst)
+}
+
+
 func Test_Marshal(t *testing.T) {
 
-	type args struct {
-		str  map[string]string
-		i    interface{} // struct initialized with default values
-		want interface{} // same type of struct with args i with expected values
-	}
-
-	tests := []struct {
-		name string
-		args args
-	} {
-		{
-			name: "Worker",
-			args: args{
-				str: map[string]string{
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
-					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "active",
-				},
-				i: struct{
-					ID       string `etcd:"id"`
-					Platform string `etcd:"platform"`
-					Status   string `etcd:"status"`
-				} {},
-				want: struct{
-					ID       string `etcd:"id"`
-					Platform string `etcd:"platform"`
-					Status   string `etcd:"status"`
-				} {
-					ID: "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					Platform: "kis",
-					Status: "active",
-				},
-			},
-		},
-		{
-			name: "Product",
-			args: args{
-				str: map[string]string{
-					"product/test.goboolean.kor": "",
-					"product/test.goboolean.kor/platform": "kis",
-					"product/test.goboolean.kor/symbol": "goboolean",
-					"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					"product/test.goboolean.kor/status": "onsubscribe",
-				},
-				i: struct{
-					ID       string `etcd:"id"`
-					Platform string `etcd:"platform"`
-					Symbol   string `etcd:"symbol"`
-					Worker 	 string `etcd:"worker"`
-					Status   string `etcd:"status"`
-				} {},
-				want: struct{
-					ID       string `etcd:"id"`
-					Platform string `etcd:"platform"`
-					Symbol   string `etcd:"symbol"`
-					Worker 	 string `etcd:"worker"`
-					Status   string `etcd:"status"`
-				} {
-					ID: "test.goboolean.kor",
-					Platform: "kis",
-					Symbol: "goboolean",
-					Worker: "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
-					Status: "onsubscribe",
-				},
-			},
-		},
-		{
-			name: "Nested Struct",
-			args: args{
-				str: map[string]string{
-					"nested/mulmuri.dev": "",
-					"nested/mulmuri.dev/detail": "",
-					"nested/mulmuri.dev/detail/name": "goboolean",
-					"nested/mulmuri.dev/detail/age": "1",
-				},
-				i: struct{
-					ID string `etcd:"id"`
-					Detail struct{
-						Name string `etcd:"name"`
-						Age int `etcd:"age"`
-					} `etcd:"detail"`
-				} {},
-				want: struct{
-					ID string `etcd:"id"`
-					Detail struct{
-						Name string `etcd:"name"`
-						Age int `etcd:"age"`
-					} `etcd:"detail"`
-				} {
-					ID: "mulmuri.dev",
-					Detail: struct{
-						Name string `etcd:"name"`
-						Age int `etcd:"age"`
-					} {
-						Name: "goboolean",
-						Age: 1,
-					},
-				},
-			},
-		},
-	}
-
-	for _, tt := range tests {
+	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			err := etcdutil.Marshal(tt.args.str, tt.args.i)
+			str, err := etcdutil.Mmarshal(tt.data)
 			assert.NoError(t, err)
-			assert.True(t, reflect.DeepEqual(tt.args.want, tt.args.i))
+			assert.True(t, reflect.DeepEqual(tt.str, str))
+		})
+	}
+}
+
+
+func Test_Unmarshal(t *testing.T) {
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			var input interface{}
+			err := deepCopy(tt.data, &input)
+			assert.NoError(t, err)
+
+			err = etcdutil.Unmarshal(tt.str, tt.model)
+			assert.NoError(t, err)
+			assert.True(t, reflect.DeepEqual(tt.data, input))
 		})
 	}
 }
