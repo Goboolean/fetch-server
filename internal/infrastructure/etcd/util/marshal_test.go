c6b63c8dfe05922ee29edeac10d3b49b001f10ed
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-30 22:49:29 +0900

    create groupby test cdode as TDD style

--- internal/infrastructure/etcd/util/marshal_test.go
+++ internal/infrastructure/etcd/util/marshal_test.go
@@ -1,14 +1,98 @@
 package etcdutil_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	etcdutil "github.com/Goboolean/fetch-system.master/internal/infrastructure/etcd/util"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
 
+
+func Test_GroupBy(t *testing.T) {
+
+	type args struct {
+		list   map[string]string
+		prefix string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []map[string]string
+	} {
+		{
+			name: "Worker",
+			args: args{
+				list: map[string]string{
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
+				},
+				prefix: "worker",
+			},
+			want: []map[string]string{
+				{
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
+				},
+				{
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
+					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
+				},
+			},
+		},
+		{
+			name: "Product",
+			args: args{
+				list: map[string]string{
+					"product/test.goboolean.kor": "",
+					"product/test.goboolean.kor/platform": "kis",
+					"product/test.goboolean.kor/symbol": "goboolean",
+					"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+					"product/test.goboolean.kor/status": "onsubscribe",
+					"product/test.goboolean.eng": "",
+					"product/test.goboolean.eng/platform": "kis",
+					"product/test.goboolean.eng/symbol": "goboolean",
+					"product/test.goboolean.eng/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+					"product/test.goboolean.eng/status": "onsubscribe",
+				},
+				prefix: "product",
+			},
+			want: []map[string]string{
+				{
+					"product/test.goboolean.kor": "",
+					"product/test.goboolean.kor/platform": "kis",
+					"product/test.goboolean.kor/symbol": "goboolean",
+					"product/test.goboolean.kor/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+					"product/test.goboolean.kor/status": "onsubscribe",
+				},
+				{
+					"product/test.goboolean.eng": "",
+					"product/test.goboolean.eng/platform": "kis",
+					"product/test.goboolean.eng/symbol": "goboolean",
+					"product/test.goboolean.eng/worker": "9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d",
+					"product/test.goboolean.eng/status": "onsubscribe",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := etcdutil.GroupBy(tt.args.list, tt.args.prefix)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
 
 
 func Test_Marshal(t *testing.T) {
@@ -122,4 +206,9 @@ func Test_Marshal(t *testing.T) {
 			assert.True(t, reflect.DeepEqual(tt.args.want, tt.args.i))
 		})
 	}
+}
+
+
+func TestCreateUuid(t *testing.T) {
+	fmt.Println(uuid.New().String())
 }
