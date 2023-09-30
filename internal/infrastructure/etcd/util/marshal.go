03bcc90f8496cd7424c74d7d33d09d065f9d05bb
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-30 23:29:53 +0900

    create groupby test code and verified it

---
+++ internal/infrastructure/etcd/util/marshal.go
@@ -0,0 +1,30 @@
+package etcdutil
+
+import (
+	"strings"
+)
+
+// GroupBy groups the list by the distinquisher that appears just after the prefix.
+// It assums that list is sorted by key.
+func GroupBy(list map[string]string, prefix string) []map[string]string {
+	var m = make(map[string]map[string]string)
+	var result []map[string]string
+
+	for k, v := range list {
+		p := strings.Split(strings.TrimPrefix(k, prefix), "/")[1]
+		if _, ok := m[p]; !ok {
+			m[p] = make(map[string]string)
+		}
+		m[p][k] = v
+	}
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
+
+
+
+func Marshal(str map[string]string, i interface{}) error {	
+	return nil
+}
--- internal/infrastructure/etcd/util/marshal_test.go
+++ internal/infrastructure/etcd/util/marshal_test.go
@@ -11,6 +11,15 @@ import (
 )
 
 
+func Contains[T any](list []T, target T) bool {
+	for _, v := range list {
+		if reflect.DeepEqual(v, target) {
+			return true
+		}
+	}
+	return false
+}
+
 
 func Test_GroupBy(t *testing.T) {
 
@@ -20,35 +29,37 @@ func Test_GroupBy(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want []map[string]string
+		name    string
+		args    args
+		want    []map[string]string
+		wantLen int
 	} {
 		{
 			name: "Worker",
 			args: args{
 				list: map[string]string{
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
 				},
 				prefix: "worker",
 			},
 			want: []map[string]string{
 				{
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
-					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
-				},
-				{
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d": "",
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/platform": "kis",
 					"worker/9cf226f7-4ee8-4a5c-9d2f-6d7c74f6727d/status": "waiting",
 				},
+				{
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886": "",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/platform": "kis",
+					"worker/b9992d7b-a926-483a-84f8-bbc05dee7886/status": "active",
+				},
 			},
+			wantLen: 2,
 		},
 		{
 			name: "Product",
@@ -83,13 +94,17 @@ func Test_GroupBy(t *testing.T) {
 					"product/test.goboolean.eng/status": "onsubscribe",
 				},
 			},
+			wantLen: 2,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := etcdutil.GroupBy(tt.args.list, tt.args.prefix)
-			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.wantLen, len(got))
+			for _, v := range got {
+				assert.True(t, Contains(tt.want, v))
+			}
 		})
 	}
 }
